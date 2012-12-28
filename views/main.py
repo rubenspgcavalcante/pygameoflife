9da466a4591d517c232781f4af6d545057a6e5dd
trix-rubens <trix-rubens@trix-rubens.(none)>
2012-12-28 09:11:49 -0300

    Main view

---
+++ views/main.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+
+from config import Config
+from resources.manager import Resource
+from helpers.singleton import singleton
+
+@singleton
+class MainView(object):
+
+    def load(self, displayController, args):
+
+        """
+        Load the main screen
+
+        arguments;
+        displayController: DisplayController instance
+        args: Dict with arguments to the view
+        """
+
+        pygame.init()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type in (QUIT, KEYDOWN):
+                    sys.exit()
+                    
+            displayController.update()
